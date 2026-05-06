c6bdecdb5d9ef9b4c3d01084690ef53c07188c6d
johbaa <johan.baarsen63@gmail.com>
2026-05-06 23:50:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ set -e
 
 cd /tmp
 
+rm -rf SIYI_PUBLIC_INSTALLER* SIYI_PI_INSTALLER* *.tar.gz
+
 wget https://github.com/johbaa/SIYI_PI_Installer/raw/main/SIYI_PI_INSTALLER_RELEASE_1.0.0.tar.gz
 
-tar -xzf SIYI_PUBLIC_INSTALLER_V78J_APT_LOCK_SAFE_BATTERY_COPY_FIX.tar.gz
+tar -xzf SIYI_PI_INSTALLER_RELEASE_1.0.0.tar.gz
 
-cd $(find . -maxdepth 1 -type d -name "SIYI_PUBLIC_INSTALLER*" | head -n 1)
+cd $(find . -maxdepth 1 -type d \( -name "SIYI_PUBLIC_INSTALLER*" -o -name "SIYI_PI_INSTALLER*" \) | head -n 1)
 
 chmod +x install.sh
 
