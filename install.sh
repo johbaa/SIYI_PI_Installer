82af7ce6a2385e1711bb083a35fc0b65d0b526d0
johbaa <johan.baarsen63@gmail.com>
2026-05-08 22:41:10 +0200

    Update installer bootstrap to 2.1.0

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ cd /tmp
 
 rm -rf SIYI_PUBLIC_INSTALLER* SIYI_PI_INSTALLER* *.tar.gz
 
-wget https://github.com/johbaa/SIYI_PI_Installer/raw/main/SIYI_PI_INSTALLER_RELEASE_2.0.1.tar.gz
+wget https://github.com/johbaa/SIYI_PI_Installer/raw/main/SIYI_PI_INSTALLER_RELEASE_2.1.0.tar.gz
 
-tar -xzf SIYI_PI_INSTALLER_RELEASE_2.0.1.tar.gz
+tar -xzf SIYI_PI_INSTALLER_RELEASE_2.1.0.tar.gz
 
 cd $(find . -maxdepth 1 -type d \( -name "SIYI_PUBLIC_INSTALLER*" -o -name "SIYI_PI_INSTALLER*" \) | head -n 1)
 
