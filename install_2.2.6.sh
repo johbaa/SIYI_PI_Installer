36edc76771aa96ba4daeb43b4558c75aae36724d
johbaa <johan.baarsen63@gmail.com>
2026-05-12 00:39:44 +0200

    Add files via upload

---
+++ install_2.2.6.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION="2.2.6"
+TARBALL="SIYI_PI_INSTALLER_RELEASE_${VERSION}.tar.gz"
+
+cd /tmp
+
+rm -rf SIYI_PUBLIC_INSTALLER_V78B
+
+wget -O "$TARBALL" "https://github.com/johbaa/SIYI_PI_Installer/raw/main/$TARBALL"
+
+tar -xzf "$TARBALL"
+
+cd SIYI_PUBLIC_INSTALLER_V78B
+
+chmod +x install.sh
+
+./install.sh
