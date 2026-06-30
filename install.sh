92619cc1a80d946ec23bb2d3974a51c58564ef2d
johbaa <johan.baarsen63@gmail.com>
2026-06-30 10:27:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 cd /home/pi
-tar -xzf SIYI_PI_INSTALLER_RELEASE_2.4.9.tar.gz
-cd SIYI_PI_INSTALLER_2.4.9
+tar -xzf SIYI_RPI_INSTALLER_RELEASE_2.4.15.tar.gz
+cd SIYI_RPI_INSTALLER_RELEASE_2.4.15
 chmod +x install.sh
 ./install.sh
