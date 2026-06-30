1a7bcda649d0bd9e9fa75e150e19fff89435f5ff
johbaa <johan.baarsen63@gmail.com>
2026-06-30 10:59:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -e
+
 cd /home/pi
+
+echo "Downloading SIYI RPi Installer 2.4.15..."
+
+curl -L -o SIYI_RPI_INSTALLER_RELEASE_2.4.15.tar.gz \
+https://github.com/johbaa/SIYI_PI_Installer/raw/main/SIYI_RPI_INSTALLER_RELEASE_2.4.15.tar.gz
+
 tar -xzf SIYI_RPI_INSTALLER_RELEASE_2.4.15.tar.gz
+
 cd SIYI_RPI_INSTALLER_RELEASE_2.4.15
 chmod +x install.sh
-./install.sh
+exec ./install.sh
