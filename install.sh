b8bfd34036cb3aee68d8e64aea6b5e0725320d00
johbaa <johan.baarsen63@gmail.com>
2026-07-08 19:36:25 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-cd /home/pi
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR"
 
-echo "Downloading SIYI RPi Installer 2.4.15..."
+tar -xzf SIYI_RPI_INSTALLER_RELEASE_2.5.0.tar.gz
 
-curl -fsSL -o SIYI_RPI_INSTALLER_RELEASE_2.4.15.tar.gz \
-https://raw.githubusercontent.com/johbaa/SIYI_PI_Installer/main/SIYI_RPI_INSTALLER_RELEASE_2.4.15.tar.gz
+cd SIYI_RPI_INSTALLER_RELEASE_2.5.0
 
-tar -xzf SIYI_RPI_INSTALLER_RELEASE_2.4.15.tar.gz
-
-cd SIYI_RPI_INSTALLER_RELEASE_2.4.15
 chmod +x install.sh
 exec ./install.sh
